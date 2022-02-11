14ccf3087f5200be253f077e7340f5b21cfe2a15
Polina <sunnyfur9@gmail.com>
2022-02-11 22:16:06 +0500

    change validation

--- js/checkFunctions.js
+++ js/checkFunctions.js
@@ -20,3 +20,36 @@ const isValidAge = (birthday) => {
         return true;
     }
 };
+
+const isCommonValid = (field) => {
+
+    const errors = [];
+    const checkValidity = (input) => {
+        const validity = input.validity;
+        if (validity.patternMismatch) {
+            errors.push('Неверный формат заполнения');
+        }
+        if (validity.rangeOverFlow) {
+            const max = input.max;
+            errors.push(`Максимальное значение не может быть больше чем ${max}`);
+        }
+        if (validity.rangeUnderFlow) {
+            const min = input.min;
+            errors.push(`Минимальное значение не может быть больше чем ${min}`);
+        }
+        if (validity.tooLong) {
+            const max = input.maxLength;
+            errors.push(`Превышен лимит введенных символов (${max})`);
+        }
+        if (validity.tooShort) {
+            const min = input.minLength;
+            errors.push(`Слишком мало символов. Минимальное количество: ${min}`);
+        }
+        if (validity.valueMissing) {
+            errors.push('Поле обязательно для заполнения');
+        }
+    };
+    checkValidity(field);
+    addDivError(field, errors.join("<br>"));
+    return (errors.length > 0) ? false : true;
+};
--- js/registrationForm.js
+++ js/registrationForm.js
@@ -1,1 +1,88 @@
-const errors = [];
+const getErrorDiv = (elem) => {
+    let rez = elem.parentElement.querySelector(".form__error");
+    if (!rez) rez = elem.parentElement.parentElement.querySelector(".form__error");
+    return rez;
+};
+
+const addDivError = (elem, message) => {
+    getErrorDiv(elem).innerHTML = message;
+};
+
+const validateAll = () => {
+
+    let valid = true;
+
+    const chekedValuesAll = document.getElementsByClassName("isRequered");
+    for (let i = 0; i < chekedValuesAll.length; i++) {
+        checkField(chekedValuesAll[i]) ? valid = false : "";
+    }
+    checkField(document.getElementById('idPrivacy')) ? valid = false : "";
+
+
+    if (!document.querySelector('input[name="nameGender"]:checked')) {
+        addDivError(document.querySelector('input[name="nameGender"'), "Поле обязательно для заполнения");
+        valid = false;
+    }
+
+    checkField(document.getElementById('idBirthDay')) ? valid = false : "";
+    checkField(document.getElementById('idEmail')) ? valid = false : "";
+    checkField(document.getElementById('idEmailCheck')) ? valid = false : "";
+    checkField(document.getElementById('idPassword')) ? valid = false : "";
+    checkField(document.getElementById('idPasswordCheck')) ? valid = false : "";
+
+    if (valid) {
+        alert(`Добро пожаловать, ${document.getElementById('idLogin').value}`);
+    } else {
+        alert('Не все поля заполнены корректно!');
+    }
+
+}
+
+const checkField = (field) => {
+    let valid = true;
+    switch (field.id) {
+        // case "idEmail":
+        //     numerr += !isValidEmail(field);
+        //     break;
+        case "idEmailCheck":
+            !isEqualFields(document.getElementById('idEmail'), document.getElementById('idEmailCheck'), "E-mail не совпадает") ? valid = false : "";
+            break;
+            // case "idPassword":
+            //     numerr += !isValidPass(field);
+            //     break;
+        case "idPasswordCheck":
+            !isEqualFields(document.getElementById('idPassword'), document.getElementById('idPasswordCheck'), "Пароль не совпадает") ? valid = false : "";
+            break;
+        case "idBirthDay":
+            !isValidAge(field) ? valid = false : "";
+            break;
+        case "idPrivacy":
+            if (field.checked) {
+                addDivError(field, "");
+            } else {
+                valid = false;
+                addDivError(field, "Нужно принять политику конфиденциальности");
+            };
+            break;
+        default:
+            addDivError(field, "");
+
+    }
+    isCommonValid(field) ? valid = false : "";
+
+    // if (!field.value && field.classList.contains("isRequered")) {
+    //     numerr++;
+    //     addDivError(field, "Поле обязательно для заполнения");
+    // };
+
+    return valid;
+};
+
+
+const buttonSubmit = document.getElementById("idSubmitRegistration");
+buttonSubmit.addEventListener("click", validateAll);
+
+
+const login = document.getElementById('formRegistration');
+login.addEventListener("keyup", (event) => checkField(event.target));
+login.addEventListener("change", (event) => checkField(event.target));
